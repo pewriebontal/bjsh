ab2a62a8e6c2a565a412549337f04b518ba8469b
Jsn267 <jasonkyaw29@gmail.com>
2024-06-03 20:21:58 +0800

    created chg_value-of-env

--- src/bjsh/env.c
+++ src/bjsh/env.c
@@ -12,6 +12,30 @@ void show_env(t_bjsh *bjsh)
     printf("\n#end_TEST#\n");
 }
 
+//var_name (e.g)USER, DBUS, ....
+//chg_value -- the value that u want to chg with
+void    chg_value_of_env(t_bjsh *bjsh,char *var_name, char *chg_value)
+{
+    int i;
+    int j;
+    char *tmp;
+
+    while (bjsh->env)
+    {
+        if (ft_strncmp(bjsh->env->value,var_name,sizeof(var_name)) == 0)
+        {
+            i = 0;
+            j = 0;
+            while (bjsh->env->value[i + 1] != '=')
+            {
+                i++;
+            }
+            //still thinking...... 
+        }
+        bjsh->env = env->next;
+    }
+}
+
 int init_env(t_bjsh *bjsh,char **env_avg)
 {
     t_env	*env;
